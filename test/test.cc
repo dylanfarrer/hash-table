f81b5b14ec3a6fb48977fe5d306b4991e079e0dd
Dylan Farrer <dylan.farrer.2019@live.rhul.ac.uk>
2024-03-15 23:05:07 +0000

    increased unit tests. currently failing with segfaults. TODO -> investigate.

--- test/test.cc
+++ test/test.cc
@@ -4,8 +4,26 @@ extern "C" {
     #include "../src/hash_table.h"
 }
 
+
+// ---------------------- Init ----------------------
+
 TEST(HashTable, InitTest) {
     ht_hash_table* ht = ht_new();
+    ht_del_hash_table(ht);
+}
+
+
+// ---------------------- Small ----------------------
+
+TEST(HashTable, SmallPut) {
+    ht_hash_table* ht = ht_new();
+    char buffer[20];
+
+    for (int i = 0; i < 10; i++) {
+        snprintf(buffer, sizeof(buffer), "%d", i);
+        ht_insert(ht, buffer, buffer);
+    }
+
     ht_del_hash_table(ht);
 }
 
@@ -35,6 +53,100 @@ TEST(HashTable, SmallPutAndSearch) {
     }
 
     for (int i = 0; i < 10; i++) {
+        snprintf(buffer, sizeof(buffer), "%d", i);
+        ASSERT_EQ(strcmp(ht_search(ht, buffer), buffer), 0);
+    }
+    ht_del_hash_table(ht);
+}
+
+
+// ---------------------- Medium ----------------------
+
+TEST(HashTable, MediumPut) {
+    ht_hash_table* ht = ht_new();
+    char buffer[20];
+
+    for (int i = 0; i < 100; i++) {
+        snprintf(buffer, sizeof(buffer), "%d", i);
+        ht_insert(ht, buffer, buffer);
+    }
+
+    ht_del_hash_table(ht);
+}
+
+TEST(HashTable, MediumPutAndDelete) {
+    ht_hash_table* ht = ht_new();
+    char buffer[20];
+
+    for (int i = 0; i < 100; i++) {
+        snprintf(buffer, sizeof(buffer), "%d", i);
+        ht_insert(ht, buffer, buffer);
+    }
+
+    for (int i = 0; i < 100; i++) {
+        snprintf(buffer, sizeof(buffer), "%d", i);
+        ht_delete(ht, buffer);
+    }
+    ht_del_hash_table(ht);
+}
+
+TEST(HashTable, MediumPutAndSearch) {
+    ht_hash_table* ht = ht_new();
+    char buffer[20];
+
+    for (int i = 0; i < 100; i++) {
+        snprintf(buffer, sizeof(buffer), "%d", i);
+        ht_insert(ht, buffer, buffer);
+    }
+
+    for (int i = 0; i < 100; i++) {
+        snprintf(buffer, sizeof(buffer), "%d", i);
+        ASSERT_EQ(strcmp(ht_search(ht, buffer), buffer), 0);
+    }
+    ht_del_hash_table(ht);
+}
+
+
+// ---------------------- Large ----------------------
+
+TEST(HashTable, LargePut) {
+    ht_hash_table* ht = ht_new();
+    char buffer[20];
+
+    for (int i = 0; i < 10000; i++) {
+        snprintf(buffer, sizeof(buffer), "%d", i);
+        ht_insert(ht, buffer, buffer);
+    }
+
+    ht_del_hash_table(ht);
+}
+
+TEST(HashTable, LargePutAndDelete) {
+    ht_hash_table* ht = ht_new();
+    char buffer[20];
+
+    for (int i = 0; i < 10000; i++) {
+        snprintf(buffer, sizeof(buffer), "%d", i);
+        ht_insert(ht, buffer, buffer);
+    }
+
+    for (int i = 0; i < 10000; i++) {
+        snprintf(buffer, sizeof(buffer), "%d", i);
+        ht_delete(ht, buffer);
+    }
+    ht_del_hash_table(ht);
+}
+
+TEST(HashTable, LargePutAndSearch) {
+    ht_hash_table* ht = ht_new();
+    char buffer[20];
+
+    for (int i = 0; i < 10000; i++) {
+        snprintf(buffer, sizeof(buffer), "%d", i);
+        ht_insert(ht, buffer, buffer);
+    }
+
+    for (int i = 0; i < 10000; i++) {
         snprintf(buffer, sizeof(buffer), "%d", i);
         ASSERT_EQ(strcmp(ht_search(ht, buffer), buffer), 0);
     }
