e24412ec9db4c2d09e3569213f0c9b7f7abcff2f
Dylan Farrer <31987488+dylanfarrer@users.noreply.github.com>
2024-03-16 19:15:53 +0000

    Overflow patch (#1)
    * increased unit tests. currently failing with segfaults. TODO -> investigate.
    
    * added temporary if statements to catch memory bug, commented on findings so far, TODO -> investigate and fix.
    
    * Fixed memory and overflow issues. TODO -> make tighter bound checks.

--- src/hash_table.c
+++ src/hash_table.c
@@ -8,15 +8,15 @@
 #include "hash_table.h"
 #include "prime.h"
 
-const unsigned long HT_PRIME_1 = 524287;
-const unsigned long HT_PRIME_2 = 104729;
+const unsigned long HT_PRIME_1 = 57885161;
+const unsigned long HT_PRIME_2 = 2147483647;
 
 static ht_item HT_DELETED_ITEM = {NULL, NULL};
 static int HT_INITIAL_BASE_SIZE = 50;
 
 static ht_item* ht_new_item(const char* key, const char* value);
 static void ht_del_item(ht_item* i);
-static int ht_hash(const char* s, const int a, const int m);
+static long ht_hash(const char* s, const int a, const int m);
 static int ht_get_hash(const char* s, const int num_buckets, const int attempt);
 static void ht_resize_up(ht_hash_table* ht);
 static void ht_resize_down(ht_hash_table* ht);
@@ -32,8 +32,8 @@ void ht_insert(ht_hash_table* ht, const char* key, const char* value) {
     if (load > 70) {
         ht_resize_up(ht);
     }
-
     ht_item* item = ht_new_item(key, value);
+
     int index = ht_get_hash(item->key, ht->size, 0);
     ht_item* cur_item = ht->items[index];
     int i = 1;
@@ -100,9 +100,7 @@ ht_hash_table* ht_new() {
 void ht_del_hash_table(ht_hash_table* ht) {
     for (int i = 0; i < ht->size; i++) {
         ht_item* item = ht->items[i];
-        if (item == &HT_DELETED_ITEM) {
-            item = NULL;
-        } else if (item != NULL) {
+        if (item != NULL && item != &HT_DELETED_ITEM) {
             ht_del_item(item);
         }
     }
@@ -140,10 +138,6 @@ static void ht_del_item(ht_item* i) {
     free(i);
 }
 
-/*
-
-*/
-
 /**
  * @brief hash a string
  * 
@@ -156,14 +150,14 @@ static void ht_del_item(ht_item* i) {
  * @param m 
  * @return int 
  */
-static int ht_hash(const char* s, const int a, const int m) {
+static long ht_hash(const char* s, const int a, const int m) {
     long hash = 0;
     const int len_s = strlen(s);
     for (int i = 0; i < len_s; i++) {
         hash += (long) pow(a, len_s - (i + 1)) * s[i];
     }
     hash = hash % m;
-    return (int) hash;
+    return hash;
 }
 
 /**
@@ -177,9 +171,9 @@ static int ht_hash(const char* s, const int a, const int m) {
  * @return int 
  */
 static int ht_get_hash(const char* s, const int num_buckets, const int attempt) {
-    const int hash_a = ht_hash(s, HT_PRIME_1, num_buckets);
-    const int hash_b = ht_hash(s, HT_PRIME_2, num_buckets);
-    return (hash_a + (attempt * (hash_b + 1))) % num_buckets;
+    const long hash_a = labs(ht_hash(s, HT_PRIME_1, num_buckets));
+    const long hash_b = labs(ht_hash(s, HT_PRIME_2, num_buckets));
+    return abs((int) (hash_a + (attempt * (hash_b + 1))) % num_buckets);
 }
 
 /**
@@ -206,18 +200,25 @@ static void ht_resize(ht_hash_table* ht, const int base_size) {
     
     // create new, larger hash table, populate it with ht items
     ht_hash_table* new_ht = ht_new_sized(base_size);
+
     for (int i = 0; i < ht->size; i++) {
         ht_item* item = ht->items[i];
-        if (item != NULL) {
+        if (item != NULL && item != &HT_DELETED_ITEM) {
             ht_insert(new_ht, item->key, item->value);
         }
     }
 
-    // set ht meta data to new_ht values
+    // transfer ownership of all values between new_ht and ht
+    // technically, count and base_size do not need to be updated to safely
+    // delete new_ht, delete if needed in future?
+    const int tmp_base_size = ht->base_size;
     ht->base_size = new_ht->base_size;
-    ht->count = new_ht->count;
+    new_ht->base_size = tmp_base_size;
 
-    // swap ht and new_ht values
+    const int tmp_count = ht->count;
+    ht->count = new_ht->count;
+    new_ht->count = tmp_count;
+
     const int tmp_size = ht->size;
     ht->size = new_ht->size;
     new_ht->size = tmp_size;
@@ -249,4 +250,3 @@ static void ht_resize_down(ht_hash_table* ht) {
     const int new_size = ht->base_size / 2;
     ht_resize(ht, new_size);
 }
-
